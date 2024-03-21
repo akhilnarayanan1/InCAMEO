ea89d36359a0d7387fd4bfe2097762b291666513
Akhil Narayanan <akhil.narayanan@se.com>
2024-03-21 16:29:24 +0530

    🩹 fix for background of menu

--- incameo-nuxt/components/Navbar.vue
+++ incameo-nuxt/components/Navbar.vue
@@ -8,7 +8,7 @@
           <div tabindex="0" role="button" class="btn btn-square btn-ghost rounded-btn">
             <span class="material-symbols-outlined">more_vert</span>
           </div>
-          <ul tabindex="0" class="menu dropdown-content z-[1] p-2 shadow-xl rounded-box w-52 mt-4">
+          <ul tabindex="0" class="menu dropdown-content bg-white dark:bg-base-200 p-2 shadow-xl rounded-box w-52 mt-4">
             <li><a>Light/Dark Mode</a></li>
             <li><a @click="signOutFacebook">Signout</a></li>
           </ul>
